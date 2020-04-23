ff9d4009e2d61c68aec16c36e79a0683b6378439
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2020-04-23 08:23:44 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,37 +4,78 @@ require 'pry'
 class PigLatinizer
 
   def piglatinize(string)
-    # single_word = !string.strip.include? " "
-    # sentence = string.strip.include? " "
     #is the word a single word or a sentence?
     #if a single word, do if statement below
-    if single_word
-      string.split("")[0].match(/^[aeiouAEIOU]/)
+    if string.split("")[0].match(/^[aeiouAEIOU]/)
       string << "way"
-    elsif !word.split("")[0].match(/[aeiouAEIOU]/)
+    elsif !string.split("")[0].match(/[aeiouAEIOU]/)
        first_consonants = string.match(/\b([^aeiouAEIOU]{1,})/)
        the_rest = string.gsub(/#{first_consonants}/, "")
        new_word = string"#{the_rest}#{first_consonants}ay"
     end
-    piglatinize_sentence(single_word)
+    piglatinize_sentence(string)
     end
   end
-#
-#
-#   def piglatinize_sentence(sentence)
-#     binding.pry
-#    sentence.split.map do |word|
-#      piglatinize(word)
-#      if
-#        word.split(" ")[0].match(/^[aeiouAEIOU]/)
-#        word << "way"
-#      elsif
-#        !word.split(" ")[0].match(/[aeiouAEIOU]/)
-#         first_consonants = word.match(/\b([^aeiouAEIOU]{1,})/)
-#         the_rest = word.gsub(/#{first_consonants}/, "")
-#         new_word = "#{the_rest}#{first_consonants}ay"
-#   end.join(" ")
-#
-# end
+
+
+  # def piglatinize_sentence(sentence)
+  #   # binding.pry
+  #  sentence.split.map do |word|
+  #    piglatinize(word)
+  #    if
+  #      word.split(" ")[0].match(/^[aeiouAEIOU]/)
+  #      word << "way"
+  #    elsif
+  #      !word.split(" ")[0].match(/[aeiouAEIOU]/)
+  #       first_consonants = word.match(/\b([^aeiouAEIOU]{1,})/)
+  #       the_rest = word.gsub(/#{first_consonants}/, "")
+  #       new_word = "#{the_rest}#{first_consonants}ay"
+  # end.join(" ")
 
 end
+
+end
+
+#
+# class PigLatinizer
+#
+#   # def vowel?(char)
+#   #   "aeiouAEIOU".include?(char)
+#   # end
+#
+#
+#   # def piglatinize(string)
+#   #   words = string.split("  ")#.to_array
+#   #   words.collect do |word|
+#   #     if vowel?(word[0])
+#   #       word += "way"
+#   #     else
+#   #       until vowel?(word[0]) do
+#   #         word = word[1..-1] + word[0]
+#   #       end
+#   #       word = word + "ay"
+#   #     end
+#   #   end.join
+#   # end
+#
+#   def piglatinize(word)
+#     word = word.split(" ")
+#     word.collect do |w|
+#       if w.split("")[0].match(/^[aeiouAEIOU]/)
+#         w << "way"
+#       else !w.split("")[0].match(/[aeiouAEIOU]/)
+#             first_consonants = word.match(/\b([^aeiouAEIOU]{1,})/)
+#             the_rest = word.gsub(/#{first_consonants}/, "")
+#             new_word = "#{the_rest}#{first_consonants}ay"
+#       end
+#     end
+#   end
+# #  # def piglatinize_string(sentence)
+#   #   words =[]
+#   #   words << sentence.split(" ")
+#   #   # words << sentence.split("  ")
+#   #   binding.pry
+#   #   items = words.collect do |w| w.piglatinize
+#   #     items.join
+#   #   end
+#   # end
